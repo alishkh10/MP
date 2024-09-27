9cf5398823d83ba9aa947b0d5c9830503a2978ca
alisher_khamidov <alisher_khamidov@EPUZTASW0126>
2024-09-27 13:51:59 +0500

    Add project files.

---
+++ Module1.Task2.ClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module1.Task2.ClassLibrary
+{
+    public static class Greeter
+    {
+        public static string GetGreeting(string username)
+        {
+            return $"{DateTime.Now}: Hello, {username}!";
+        }
+    }
+}
---
+++ Module1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Module1.Task2.ClassLibrary;
+
+if (args.Length > 0)
+{
+    Console.WriteLine(Greeter.GetGreeting(args[0]));
+}
+else
+{
+    Console.WriteLine("Username argument is missing.");
+}
